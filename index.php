de77401d4fee884ff3a9434256960aff25bbfd1a
simplu27 <simplu27@yahoo.com>
2016-01-20 16:59:31 +0200

    Added page variabiles, include header

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
+// index.php Home page
+include_once $_SERVER['DOCUMENT_ROOT'].'/_inc/config.php';
 
-// index.php
+$pageId = "homePage";
+$pageTitle = "Title";
+$pageDescription = "Page Description";
+$pageKeywords = "keywords, here";
+
+include_once ("_inc/header.php");
