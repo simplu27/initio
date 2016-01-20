45f898669a2816fa8cb113490a6179cd85f92f12
simplu27 <simplu27@yahoo.com>
2016-01-20 20:09:33 +0200

    Populated list

--- _inc/menu.php
+++ _inc/menu.php
@@ -1,4 +1,9 @@
 <?php
 // _inc/menu.php
 ?>
-Menu here
+<ul>
+	<li><a href="/">Home</a></li>
+	<li><a href="/page1">Page 1</a></li>
+	<li><a href="/page2">Page 2</a></li>
+	<li><a href="/page3">Page 3</a></li>
+</ul>
