5ccb2fa759dce39f21b7ac3c913c2074da8f7378
simplu27 <simplu27@yahoo.com>
2016-01-20 20:03:32 +0200

    include js scripts and closing body and html

--- _inc/footer.php
+++ _inc/footer.php
@@ -1,2 +1,12 @@
 <?php
-// _inc/footer.php
+// _inc/footer.php ?>
+
+<footer id="footer">
+	This is the footer!
+</footer>
+
+<script type="text/javascript" src="/_js/jquery.js"></script>
+<script type="text/javascript" src="/_js/script.js"></script>
+
+</body>
+</html>
