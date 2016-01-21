1db40503a654bc32992d1b17ea48dcd84c6f08ff
simplu27 <simplu27@yahoo.com>
2016-01-21 15:02:04 +0200

    copyright year function

--- _inc/footer.php
+++ _inc/footer.php
@@ -1,7 +1,14 @@
 <footer id="footer">
 	This is the footer!
 	<div id="copyright">
-		Copyright &copy; 2015 - <?php echo date('Y'); ?> All rights reserved.
+		Copyright &copy; <?php 
+		$start_year = "2015";
+		if (date('Y') == $start_year) {
+			echo $start_year;
+		} else {
+			echo $start_year . " - " . date('Y');
+		}
+		?> All rights reserved.
 	</div><!-- copyright -->
 </footer>
 
